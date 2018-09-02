f6a91d55097454c19b20835918bec0414cfea415
George K. Thiruvathukal <gkt@cs.luc.edu>
2018-09-01 23:21:14 -0500

    add support for words on stdin, sleeping between writing output lines, and SIGPIPE handling

--- wc_window.py
+++ wc_window.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
+# Note: The work of processing the indefinite sequence of words (via a Python
+# generator) is done in wc_on_geneator(). The rest of the code is about making
+# this a nice-to-use program!
+
 import argparse
 import re
+import os
+import sys
+import time
 
 from collections import deque
 from urllib import request
@@ -10,14 +17,24 @@ from urllib import request
 # Try -u http://www.gutenberg.org/files/1524/1524-0.txt
 
 
-def get_text_from_url(url):
-    resource = request.urlopen(url)
-    return resource.read().decode("utf-8")
-
-
 def get_words(text):
     word_pattern = re.compile("\w+")
     return re.findall(word_pattern, text)
+
+
+def get_words_from_url(url):
+    resource = request.urlopen(url)
+    text = resource.read().decode("utf-8")
+    return get_words(text)
+
+
+def get_words_from_stdin():
+    while True:
+        line = sys.stdin.readline()
+        if not line:
+            break
+        for word in get_words(line):
+            yield word
 
 
 def wc_on_generator(word_generator, window_size):
@@ -42,35 +59,57 @@ def show_counts(wc, top):
     return ", ".join(["%(word)s: %(count)d" % vars() for (count, word) in items])
 
 
-def parse_args():
+def get_argparser():
     parser = argparse.ArgumentParser(description="Word Count / Sliding Stream")
     parser.add_argument("-w", "--window_size",
                         type=int, default=100, help="window size (in words)")
-    parser.add_argument("-H", "--head",
+    parser.add_argument("-n", "--numlines",
                         type=int, default=10, help="number of lines to process")
     parser.add_argument("-t", "--top",
                         type=int, default=5, help="show top ranked (number of) items")
+    parser.add_argument("-z", "--zzz", type=float, help="sleep for seconds (float allowed, e.g. 0.05) between writing output lines", default=0)
 
     group = parser.add_mutually_exclusive_group()
     group.add_argument("-u", "--url",
                        type=str, default=None, help="url to process")
-    group.add_argument("-p", "--pipe", action="store_true",
-                       default=False, help="pipe from standard input")
-    return parser.parse_args()
+    group.add_argument("-i", "--stdin", action="store_true",
+                       default=False, help="get text from standard input")
+    return parser
 
+
+# See https://docs.python.org/3/library/signal.html. Python provides full
+# support for POSIX in the os module and full support for signal handling.
+# Only output pipes require you to address borken pipes, which is also needed
+# in C programming.
 
 def main():
-    args = parse_args()
+    arg_parser = get_argparser()
+    args = arg_parser.parse_args()
 
-    text = ''  # by default, no words to process
     if args.url:
-        text = get_text_from_url(args.url)
+        words = get_words_from_url(args.url)
+    elif args.stdin:
+        words = get_words_from_stdin()
+    else:
+        arg_parser.print_help()
+        sys.stderr.write("\n--url or --stdin not specified (aborting)\n")
+        sys.exit(1)
 
-    words = get_words(text)
     wc_generator = wc_on_generator(words, args.window_size)
-    for window in range(0, args.head):
-        print("%d:" % window, show_counts(next(wc_generator), args.top))
-        print()
+
+    # This try/except is straight out of the Python docs (for output SIGPIPE)
+
+    try:
+        for window in range(0, args.numlines):
+            sys.stdout.write("%d:" % window)
+            sys.stdout.write(show_counts(next(wc_generator), args.top) + '\n')
+            sys.stdout.flush()
+            if args.zzz:
+                time.sleep(args.zzz)
+    except BrokenPipeError:
+        devnull = os.open(os.devnull, os.O_WRONLY)
+        os.dup2(devnull, sys.stdout.fileno())
+        sys.exit(1)  # Python exits with error code 1 on EPIPE
 
 
 if __name__ == '__main__':
